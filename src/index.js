7d13bbc9d976efb48c3326bdedb9e1ba3ecc8a2e
teme <teemu.taponen22@gmail.com>
2022-02-07 10:32:26 +0200

    kkkkk

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import LunchMenu from '/sodexo.json';
 // Test
 console.log('lunch menu object', LunchMenu);
-let language = true;
+
 let texten = "";
 let textfi = "";
 document.getElementById("lang").addEventListener("click", changeLang);
-document.getElementById('sort').addEventListener("click", sortSort);
+
+
 
 for(let i = 1; i < Object.keys(LunchMenu.courses).length; i++){
     let obj = Object(LunchMenu.courses[i]);
     console.log(obj);
     texten += obj.title_en + '<br> price: ' + obj.price + '<br>';
     textfi += obj.title_fi + '<br> price: ' + obj.price + '<br>';
+    document.getElementById("listfi").innerHTML = texten;
 }
+let language = textfi;
 
 function changeLang(){
     language = !language;
@@ -20,7 +23,7 @@ function changeLang(){
                       document.getElementById("listfi").innerHTML = texten;
                       document.getElementById("list").innerHTML = "";
                     } else {
-                      document.getElementById("list").innerHTML = textfi;
-                      document.getElementById("listfi").innerHTML = "";
+                      document.getElementById("listfi").innerHTML = textfi;
+                      document.getElementById("list").innerHTML = "";
                     }
 }
